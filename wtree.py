b03b9376a6ed1dcb7bcef8dd3cdab69f74735389
sjk5766 <wjdrnr6662@gmail.com>
2020-05-12 17:08:23 +0900

    init, valid 체크 함수 추가

--- wtree.py
+++ wtree.py
@@ -1,1 +1,32 @@
-# 
+import os
+import sys
+
+arg_len = len(sys.argv)
+
+def is_valid():
+    if(arg_len > 2):
+        print_usage()
+        return False
+    return True
+
+def print_usage():
+    print("usage: ")
+
+def init():
+    option = ''
+    if(arg_len > 1):
+        option = getOption()
+    return option
+
+        
+def getOption():
+    return {
+        "-L": "-L"
+    }.get(sys.argv[1])
+
+if(is_valid()):
+    option = init()
+    #print(option)
+
+    
+
