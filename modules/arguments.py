64d36e98a40cd8261a77142833c3873f57c872fd
sjk5766 <wjdrnr6662@gmail.com>
2020-05-15 10:46:45 +0900

    기능 모듈화

---
+++ modules/arguments.py
@@ -0,0 +1,83 @@
+import sys
+from .constants import valid_opts
+
+def is_args_valid(arg_len):
+    if _is_len_valid(arg_len):
+        return True
+        
+    print_usage()
+    return False
+
+_is_len_valid = lambda arg_len: arg_len < 5
+
+def print_usage(): # 기능 개발 후 최종 추가
+    print("usage: ")
+
+def parse_args(arg_len):
+    args = {"path": None, "opt": None, "extra": None}
+
+    if arg_len == 1:
+        args['path'] = "."
+    else:
+        args['path'], args['opt'], args['extra'] = _parse_args(arg_len)
+    return args
+
+def _parse_args(arg_len):
+
+    path = "."
+    opt = None
+    extra = None
+
+    if arg_len == 2:  
+        if '-' in sys.argv[1]:
+            opt = sys.argv[1]
+        else:
+            path = sys.argv[1]
+
+    elif arg_len == 3:
+        if '-' in sys.argv[1]:
+            if 'L' in sys.argv[1]:
+                opt = '-L'
+                extra = sys.argv[2]
+            else:
+                opt = sys.argv[1]
+                path = sys.argv[2]
+        else:
+            path = sys.argv[1]
+            opt = sys.argv[2]
+
+    elif arg_len == 4:
+        if '-L' == sys.argv[1]:
+            opt = '-L'
+            extra = sys.argv[2]
+            path = sys.argv[3]
+        else:
+            opt = '-L'
+            extra = sys.argv[3]
+            path = sys.argv[1]
+
+    return path, opt, extra
+
+def is_each_value_valid(args):
+    path, opt, extra = args['path'], args['opt'], args['extra']
+    if _path_check(path) and _opt_check(opt) and _extra_check(opt, extra):
+        return True
+    return False
+    
+def _path_check(path):
+    if path == '.' or isinstance(path, str):
+        return True
+    return False
+
+def _opt_check(opt):
+    if opt == None or opt in valid_opts:
+        return True
+    return False
+
+def _extra_check(opt, extra):
+    try:
+        if (opt != '-L' and extra == None) or (opt == '-L' and int(extra)):
+            return True
+    except:
+        return False
+    return False
--- wtree.py
+++ wtree.py
@@ -1,95 +1,21 @@
 import os
 import sys
-
+from modules.arguments import *
 arg_len = len(sys.argv)
-valid_opts = ['-L', '-d', '-f']
 
-def is_args_valid():
-    if _is_len_valid():
-        return True
-        
-    print_usage()
-    return False
 
-_is_len_valid = lambda: arg_len < 5
-
-def print_usage(): # 기능 개발 후 최종 추가
-    print("usage: ")
-
-def parse_args():
-    args = {"path": None, "opt": None, "extra": None}
-
-    if arg_len == 1:
-        args['path'] = "."
-    else:
-        args['path'], args['opt'], args['extra'] = _parse_args()
-    return args
-
-def _parse_args():
-
-    path = "."
-    opt = None
-    extra = None
-
-    if arg_len == 2:  
-        if '-' in sys.argv[1]:
-            opt = sys.argv[1]
-        else:
-            path = sys.argv[1]
-
-    elif arg_len == 3:
-        if '-' in sys.argv[1]:
-            if 'L' in sys.argv[1]:
-                opt = '-L'
-                extra = sys.argv[2]
-            else:
-                opt = sys.argv[1]
-                path = sys.argv[2]
-        else:
-            path = sys.argv[1]
-            opt = sys.argv[2]
-
-    elif arg_len == 4:
-        if '-L' == sys.argv[1]:
-            opt = '-L'
-            extra = sys.argv[2]
-            path = sys.argv[3]
-        else:
-            opt = '-L'
-            extra = sys.argv[3]
-            path = sys.argv[1]
-
-    return path, opt, extra
-
-def is_each_value_valid(args):
-    path, opt, extra = args['path'], args['opt'], args['extra']
-    if _path_check(path) and _opt_check(opt) and _extra_check(opt, extra):
-        return True
-    return False
-    
-def _path_check(path):
-    if path == '.' or isinstance(path, str):
-        return True
-    return False
-
-def _opt_check(opt):
-    if opt == None or opt in valid_opts:
-        return True
-    return False
-
-def _extra_check(opt, extra):
-    try:
-        if (opt != '-L' and extra == None) or (opt == '-L' and int(extra)):
-            return True
-    except:
-        return False
-    return False
-
-if is_args_valid():
-    args = parse_args()
+if is_args_valid(arg_len):
+    args = parse_args(arg_len)
     if is_each_value_valid(args):
-        pass
         current_path = "C:\\Users\\MEMENT\\Desktop\\side_project\\study\\principle" #os.getcwd()
+        """
+        for path, dirs, files in os.walk(current_path):
+            print(path.split(os.path.sep)[-1])
+            print(dirs)
+            print(path)
+            
+            print(files)
+            print('===')
         """
         with os.scandir(current_path) as entries:
             for entry in entries:
@@ -100,15 +26,7 @@ if is_args_valid():
                     print('file:' + entry.name)
                 if entry.is_dir():
                     print('dir:' + entry.name)
-            
         
-        for path, dirs, files in os.walk(current_path):
-            print(dirs)
-            print(path)
-            
-            #print(files)
-            print('===')
-        """
     else:
         print_usage()
         
