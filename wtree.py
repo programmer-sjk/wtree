119eabb598d92039c72aae5fb3556aed572af7c0
sjk5766 <wjdrnr6662@gmail.com>
2020-05-16 23:18:57 +0900

    디렉토리 및 파일 출력 함수 개발

--- wtree.py
+++ wtree.py
@@ -3,29 +3,63 @@ import sys
 from modules.arguments import *
 arg_len = len(sys.argv)
 
+CONNECT_CHAR = '──'
+CONNECT_CHAR_MORE = '├─'
+END_CONNECT_CHAR = '└─'
+ROOT_VERTI_LINE = '│'
+
+def rescursive_print(path, depth = 1, is_last = False):
+    indent = '  '
+    files = []
+    dirs = []
+
+    with os.scandir(path) as entries:
+        total_length = sum(1 for e in entries)
+
+    with os.scandir(path) as entries:
+        for idx, entry in enumerate(entries):
+            if entry.is_file():
+                files.append(entry.name)
+            if entry.is_dir():
+                dirs.append(entry.name)
+
+    for idx, name in enumerate(files):
+        if depth == 1:
+            print(CONNECT_CHAR_MORE + CONNECT_CHAR * depth + name)
+        elif idx == 0:
+            if len(files) == 1:
+                print(ROOT_VERTI_LINE + indent * depth + END_CONNECT_CHAR + name)
+            else:
+                print(ROOT_VERTI_LINE + indent * depth + CONNECT_CHAR_MORE + name)
+        elif len(dirs) == 0 and idx + 1== len(files):
+            print(ROOT_VERTI_LINE + indent * depth + END_CONNECT_CHAR + name)
+        else:
+            print(ROOT_VERTI_LINE + indent * depth + CONNECT_CHAR_MORE + name)
+                
+    for idx, name in enumerate(dirs):
+        if depth == 1:
+            print_blue(CONNECT_CHAR_MORE + CONNECT_CHAR * depth, name)
+        elif idx == 0:
+            print_blue(ROOT_VERTI_LINE + indent * depth + END_CONNECT_CHAR, name)
+        else:
+            print_blue(CONNECT_CHAR_MORE + CONNECT_CHAR * depth , name)         
+        
+        depth = depth + 1
+        print(idx + 1 == len(dirs), name)
+        rescursive_print(path + '/' + name, depth, idx + 1 == len(dirs))
+        depth = depth - 1
+
+def print_blue(plain, dirname):
+    (BLUE_COLOR, END_COLOR) = ('\033[94m', '\033[0m')
+    print(plain + BLUE_COLOR + dirname + END_COLOR)        
 
 if is_args_valid(arg_len):
     args = parse_args(arg_len)
     if is_each_value_valid(args):
         current_path = "C:\\Users\\MEMENT\\Desktop\\side_project\\study\\principle" #os.getcwd()
-        """
-        for path, dirs, files in os.walk(current_path):
-            print(path.split(os.path.sep)[-1])
-            print(dirs)
-            print(path)
-            
-            print(files)
-            print('===')
-        """
-        with os.scandir(current_path) as entries:
-            for entry in entries:
-                print(entry.name)
-            
-            for entry in entries:
-                if entry.is_file():
-                    print('file:' + entry.name)
-                if entry.is_dir():
-                    print('dir:' + entry.name)
+        print(args['path'])          
+        
+        rescursive_print(current_path)
         
     else:
         print_usage()
