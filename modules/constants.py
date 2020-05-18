bc577231701e404439addbf24bc83e68100edae4
sjk5766 <wjdrnr6662@gmail.com>
2020-05-18 17:19:52 +0900

    가독성을 위한print 인자 통일

--- modules/constants.py
+++ modules/constants.py
@@ -1,1 +1,5 @@
 valid_opts = ['-L', '-d', '-f']
+CONNECT_CHAR = '──'
+CONNECT_CHAR_MORE = '├─'
+CONNECT_CHAR_END = '└─'
+CONNECT_CHAR_ROOT = '│'
--- wtree.py
+++ wtree.py
@@ -1,19 +1,17 @@
 import os
 import sys
 from modules.arguments import *
+from modules.constants import CONNECT_CHAR, CONNECT_CHAR_MORE, CONNECT_CHAR_END, CONNECT_CHAR_ROOT
 arg_len = len(sys.argv)
 
-CONNECT_CHAR = '──'
-CONNECT_CHAR_MORE = '├─'
-END_CONNECT_CHAR = '└─'
-ROOT_VERTI_LINE = '│'
 dir_count = 0
 
 def rescursive_print(path, last_dir_idx, depth = 1):
     global dir_count
-    indent = '  '
-    files = []
-    dirs = []
+    (space, dspace) = (' ', '  ')
+    (files, dirs) = ([], [])
+    root_char = connect_char = ''
+    
 
     with os.scandir(path) as entries:
         for idx, entry in enumerate(entries):
@@ -24,34 +22,39 @@ def rescursive_print(path, last_dir_idx, depth = 1):
 
     for idx, name in enumerate(files):
         if depth == 1:
-            print(CONNECT_CHAR_MORE + CONNECT_CHAR * depth + name)
+            (root_char, connect_char) = (CONNECT_CHAR_MORE, CONNECT_CHAR * depth)
         elif dir_count == last_dir_idx:
             if len(files) == 1 or idx + 1== len(files):
-                print(' ' + indent * depth + END_CONNECT_CHAR + name)
+                (root_char, connect_char) = (space, dspace * depth + CONNECT_CHAR_END)
             else:
-                print(' ' + indent * depth + CONNECT_CHAR_MORE + name)
+                (root_char, connect_char) = (space, dspace * depth + CONNECT_CHAR_MORE)
         elif idx == 0:
             if len(files) == 1:
-                print(ROOT_VERTI_LINE + indent * depth + END_CONNECT_CHAR + name)
+                (root_char, connect_char) = (CONNECT_CHAR_ROOT, dspace * depth + CONNECT_CHAR_END)
             else:
-                print(ROOT_VERTI_LINE + indent * depth + CONNECT_CHAR_MORE + name)
+                (root_char, connect_char) = (CONNECT_CHAR_ROOT, dspace * depth + CONNECT_CHAR_MORE)
         elif len(dirs) == 0 and idx + 1== len(files):
-            print(ROOT_VERTI_LINE + indent * depth + END_CONNECT_CHAR + name)
+            (root_char, connect_char) = (CONNECT_CHAR_ROOT, dspace * depth + CONNECT_CHAR_END)
         else:
-            print(ROOT_VERTI_LINE + indent * depth + CONNECT_CHAR_MORE + name)
-                
+            (root_char, connect_char) = (CONNECT_CHAR_ROOT, dspace * depth + CONNECT_CHAR_MORE)
+
+        print(root_char + connect_char + name)
+
+
     for idx, name in enumerate(dirs):
         if depth == 1:
             if idx + 1 == last_dir_idx:
-                print_blue(END_CONNECT_CHAR + CONNECT_CHAR * depth, name)
-            else:    
-                print_blue(CONNECT_CHAR_MORE + CONNECT_CHAR * depth, name)
+                (root_char, connect_char) = (CONNECT_CHAR_END, CONNECT_CHAR * depth)
+            else:
+                (root_char, connect_char) = (CONNECT_CHAR_MORE, CONNECT_CHAR * depth)    
             dir_count = dir_count + 1
         elif idx == 0:
-            print_blue(ROOT_VERTI_LINE + indent * depth + END_CONNECT_CHAR, name)
+            (root_char, connect_char) = (CONNECT_CHAR_ROOT, dspace * depth + CONNECT_CHAR_END)    
         else:
-            print_blue(CONNECT_CHAR_MORE + CONNECT_CHAR * depth , name)         
+            (root_char, connect_char) = (CONNECT_CHAR_MORE, CONNECT_CHAR * depth)    
+            
         
+        print_blue(root_char + connect_char, name)
         depth = depth + 1
         rescursive_print(path + '/' + name, last_dir_idx, depth)
         depth = depth - 1
@@ -62,13 +65,13 @@ def print_blue(plain, dirname):
 
 def get_last_dir_index(path):
     with os.scandir(path) as entries:
-        return sum(1 for e in entries)
+        return sum(1 for e in entries if e.is_dir())
 
 
 if is_args_valid(arg_len):
     args = parse_args(arg_len)
     if is_each_value_valid(args):
-        current_path = "C:\\Users\\user\\Desktop\\side_project\\study\\principle" #os.getcwd()
+        current_path = "C:\\Users\\Mement\\Desktop\\side_project\\study\\principle" #os.getcwd()
         print(args['path'])          
         last_dir_idx = get_last_dir_index(current_path)
         rescursive_print(current_path, last_dir_idx)
