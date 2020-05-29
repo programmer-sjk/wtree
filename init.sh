0c66c605d6803a4e571cfd9c286c3672c995e92d
sjk5766 <wjdrnr6662@gmail.com>
2020-05-29 12:28:21 +0900

    init.sh -b 옵션을 주면 exe program build

--- init.sh
+++ init.sh
@@ -1,1 +1,16 @@
+binary_mode=0
+
+while getopts "b" opt; do
+    case "$opt" in
+    b) binary_mode=1
+       echo "wtree is installed executed program..."
+    esac
+done
+
 pip install colorama
+
+if [ "${binary_mode}" -eq 1 ]; then
+	pip install pyinstaller
+fi
+
+pyinstaller.exe wtree.py
